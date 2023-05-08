dab26452482b15981377991e076bf8ba4441d315
Michael <usermike671@gmail.com>
2023-05-08 11:59:22 -0700

    React setup - sass, bootstrap & react-router-dom installed

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.appContainer}>
+      <h1>Guam NFT</h1>
+    </div>
+  );
+}
+
+export default App;
