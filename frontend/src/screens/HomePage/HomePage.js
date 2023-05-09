d7fbf700fd7b3de6fe803dc747e6166caa20cb8a
Michael <usermike671@gmail.com>
2023-05-08 22:32:37 -0700

    added pages & link scroll - card, commandbar done

---
+++ frontend/src/screens/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import styles from './homePage.module.scss';
+
+const HomeScreen = () => {
+  return (
+    <div id={'home'} className={styles.homeScreen}>
+      <h1>HomeScreen</h1>
+    </div>
+  )
+}
+
+export default HomeScreen;
